cc5ecb98be5689731b8a246f5f29111fd0fab8e6
Gjermund Persson Myrvang <gjermupm@uio.no>
2024-09-11 14:50:24 +0200

    add header component and add to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Header />
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -77,5 +81,8 @@ export default function Home() {
         </a>
       </footer>
     </div>
+      </div>
+    </main>
+      
   );
 }
