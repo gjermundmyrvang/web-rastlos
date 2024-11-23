304cb84ee6cb33d14c31820c9b8a2a78e65264a3
gjermundmyrvang <myrvang.gjermund@gmail.com>
2024-11-23 20:11:50 +0100

    remove z index causing issues

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ export const Header = () => {
   return (
     <div className="pb-20 pt-36">
         <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen z-10" fill="blue"/>
-        <Spotlight className="top-10 left-full h-[80vh] w-[50vw] z-10" fill="orange"/>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="blue"/>
+        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="orange"/>
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="orange"/>
         </div>
 
@@ -15,7 +15,7 @@ export const Header = () => {
                 <h1 className="uppercase tracking-widest text-xs sm:text-lg md:text-xl text-center text-blue-100 max-w-80">
                     Den beste foreningen på IFI noensinne!
                 </h1>
-                <Image src={"/assets/mainlogoW.png"} width={500} height={500} layout="responsive" alt="logo av ifirastløs" className="z-0"/>
+                <Image src={"/assets/mainlogoW.png"} width={500} height={500} layout="responsive" alt="logo av ifirastløs"/>
             </div>
         </div>
     </div>
